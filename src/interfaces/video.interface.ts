2c7ce5388a7612079b8f4b9f1644c5e549794896
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-06-28 02:44:25 +0530

    updated video interface

--- src/interfaces/video.interface.ts
+++ src/interfaces/video.interface.ts
@@ -1,15 +1,16 @@
 import { Document } from 'mongoose';
 
+/** TODO: might need to store child comment schema for userId and string */
 export interface IVideo extends Document {
   videoId: string;
   thumbnail: string;
   title: string;
   description: string;
+  channelName: string;
   channelLogo: string;
   length: string;
   views: number;
   likes: number;
-  /** TODO: might need to store child comment schema for userId and string */
   comments: string[];
   category: string[];
   date: string;
