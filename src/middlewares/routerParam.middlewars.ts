01592ea72f0df1be5a281cf068eef49c50d67b7a
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-06-28 02:41:08 +0530

    added routerParam middleware

--- src/middlewares/routerParam.middlewars.ts
+++ src/middlewares/routerParam.middlewars.ts
@@ -1,30 +1,26 @@
 import consola from 'consola';
-import { IRequest, IResponse, INextFunction } from '@src/interfaces/express.interface';
 import { User } from '@src/models/user.model';
 import { Video } from '@src/models/video.model';
 import { Playlist } from '@src/models/playlist.model';
+import { resJson } from '@src/utils/responseHelpers';
+import { IRequest, IResponse, INextFunction } from '@src/interfaces/express.interface';
 
 /**
  * User controller for router.param middleware
  */
 export const findUserById = async (req: IRequest, res: IResponse, next: INextFunction): Promise<void> => {
-  const userId = req.params;
+  const { userId } = req.params;
   try {
     const user = await User.findById(userId);
     if (user) {
       req.user = user;
+      consola.success('User found by userId');
     } else {
-      res.status(400).send({ success: false, message: `User Not Present with ${userId}` });
+      resJson(res, 404, false, `User Not Found with ${userId}`);
     }
-    consola.success('User found by userId');
     next();
-  } catch (err) {
-    consola.error(new Error('Unable to find User by userId'), err);
-    res.status(400).json({
-      success: false,
-      message: 'Unable to retrive the User by userId',
-      error: err,
-    });
+  } catch (error) {
+    resJson(res, 500, false, `Unable to find User with ${userId}`, error);
   }
 };
 
@@ -32,22 +28,18 @@ export const findUserById = async (req: IRequest, res: IResponse, next: INextFun
  * Video controller for router.param middleware
  */
 export const findVideoById = async (req: IRequest, res: IResponse, next: INextFunction): Promise<void> => {
-  const videoId = req.params;
+  const { videoId } = req.params;
   try {
     const video = await Video.findById(videoId);
     if (video) {
       req.video = video;
+      consola.success('Video found by videoId');
     } else {
-      res.status(400).send({ success: false, message: `video Not Present with ${videoId}` });
+      resJson(res, 404, false, `video Not Found with ${videoId}`);
     }
     next();
   } catch (error) {
-    consola.error(new Error('Unable to find Video by videoId'), error);
-    res.status(400).json({
-      success: false,
-      message: 'Unable to find Video by videoId',
-      error,
-    });
+    resJson(res, 500, false, `Unable to find video with ${videoId}`, error);
   }
 };
 
@@ -55,21 +47,17 @@ export const findVideoById = async (req: IRequest, res: IResponse, next: INextFu
  * Playlist controller for router.param middleware
  */
 export const findPlaylistById = async (req: IRequest, res: IResponse, next: INextFunction): Promise<void> => {
-  const playlistId = req.params;
+  const { playlistId } = req.params;
   try {
     const playlist = await Playlist.findById(playlistId);
     if (playlist) {
       req.playlist = playlist;
+      consola.success('Playlist found by playlistId');
     } else {
-      res.status(400).send({ success: false, message: `playlist Not Present with ${playlistId}` });
+      resJson(res, 404, false, `playlist Not Found with ${playlistId}`);
     }
     next();
   } catch (error) {
-    consola.error(new Error('Unable to find Playlist by playlistId'), error);
-    res.status(400).json({
-      success: false,
-      message: 'Unable to find Playlist by playlistId',
-      error,
-    });
+    resJson(res, 500, false, `Unable to find playlistId with ${playlistId}`, error);
   }
 };
