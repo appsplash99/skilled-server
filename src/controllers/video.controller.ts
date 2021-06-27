4858d17d0a6bd6241500edf578cfa292ed164389
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-06-28 02:39:34 +0530

    updated video routes and controller

--- src/controllers/video.controller.ts
+++ src/controllers/video.controller.ts
@@ -2,15 +2,9 @@
 /**
  * DO NOT REMOVE THE DISABLED ESLINT RULES ABOVE
  */
-// import mongoose from 'mongoose';
 import { Video } from '@src/models/video.model';
 import { resJson } from '@src/utils/responseHelpers';
 import { IRequest, IResponse } from '@src/interfaces/express.interface';
-/** */
-// import { PSYCHOLOGY_VIDEOSDATA as psycho } from '@src/config/fakerDB/psychology.videos';
-// import { TALKS_VIDEOSDATA as talks } from '@src/config/fakerDB/tedXTalks.videos';
-// import { SELFHELP_VIDEOSDATA as selfHelp } from '@src/config/fakerDB/selfHelp.videos';
-/** */
 
 export const getAllVideos = async (req: IRequest, res: IResponse): Promise<void> => {
   try {
@@ -30,23 +24,3 @@ export const getOneVideo = async (req: IRequest, res: IResponse): Promise<void> 
     resJson(res, 500, true, 'Unable to get desired Video', error);
   }
 };
-
-// /**
-//  * TODO: RE<PVE CODE BELOW
-//  * ===================================
-//  * ADD DOCUMENTS INTO DATABASE AT ONCE
-//  * ===================================
-//  */
-// export const populateMyVideosInDb = async (req: IRequest, res: IResponse): Promise<void> => {
-//   try {
-//     console.log({ req, message: 'ran populateMyVideosInDb' });
-//     const combinedVideosArray = [...talks, ...psycho, ...selfHelp];
-//     const combinedVideosArrayWithId = combinedVideosArray.map(videoObj => {
-//       return { ...videoObj, _id: mongoose.Types.ObjectId() };
-//     });
-//     const databaseVideos = await Video.insertMany(combinedVideosArrayWithId);
-//     resJson(res, 201, true, 'successfully ran populateMyVideosInDb', 'no error', databaseVideos);
-//   } catch (error) {
-//     resJson(res, 500, false, 'Unable to run populateMyVideosInDb', error);
-//   }
-// };
--- src/routes/video.router.ts
+++ src/routes/video.router.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { findVideoById, findPlaylistById } from '@src/middlewares/routerParam.middlewars';
-import { getAllVideos, getOneVideo, populateMyVideosInDb } from '@src/controllers/video.controller';
+import { getAllVideos, getOneVideo } from '@src/controllers/video.controller';
 
 const router = Router();
 
@@ -9,7 +9,5 @@ router.param('playlistId', findPlaylistById);
 
 router.get('/', getAllVideos);
 router.get('/:videoId', getOneVideo);
-
-router.post('/post-videos', populateMyVideosInDb);
 
 export { router as videoRoutes };
