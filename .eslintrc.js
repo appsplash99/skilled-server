5bb5fac75853ee85541ae399ecbf5655c991065a
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-12 07:15:04 +0530

    updated import path config

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,19 +47,4 @@ module.exports = {
       },
     ],
   },
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts'],
-    },
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-
-        // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json by default
-
-        // use <root>/path/to/folder/tsconfig.json
-        project: './tsconfig.json',
-      },
-    },
-  },
 };
