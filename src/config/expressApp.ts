b62f21982cff80e6716136d7a9880229ddf4e993
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-12 06:25:24 +0530

    updated express app config

--- src/config/expressApp.ts
+++ src/config/expressApp.ts
@@ -1,22 +1,30 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import { router as apiRoutes } from '@src/routes';
-import { catchAllErrorHandler as errorHandler } from '@src/middlewares/catchAllErrors.middleware';
-import { routeNotFoundHandler as routeNotFound } from '@src/middlewares/routeNotFound.middleware';
+import { apiRoutes } from '@src/routes';
+import { errorHandler } from '@src/middlewares/catchAllErrors.middleware';
+import { routeNotFound } from '@src/middlewares/routeNotFound.middleware';
+import { IRequest, IResponse } from '@src/interfaces/express.interface';
 
 // initialize express app
-export const app = express();
+const app = express();
 
 // middlewares
 app.use(cors()); // enable CORS - Cross Origin Resource Sharing
 app.use(bodyParser.json()); // parse body params and attache them to req.body
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req: IRequest, res: IResponse) => {
+  res.send('Express Typescript Server for Skilled Video Library Client-React ');
+});
+
 // mount api v1 routes
 app.use('/api/', apiRoutes);
 
-// errorHandler
-app.use(errorHandler);
 // path not found handler
 app.use(routeNotFound);
+
+// errorHandler
+app.use(errorHandler);
+
+export { app as expressApp };
