8035fffee9170b7f70e7cf8e2a35bf5e28e70b18
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-12 06:24:55 +0530

    added  jwtSecret

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,4 +4,5 @@ dotenv.config();
 
 export const env = process.env.NODE_ENV;
 export const port = process.env.PORT;
+export const jwtSecret = process.env.JWT_SECRET as string;
 export const mongoVideoLibDbUri = process.env.MONGO_VIDEO_LIB_DB_URI as string;
